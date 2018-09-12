fe82e7272b977b99aeb45680ce821e1e11cbac19
kdominik <k-dominik@users.noreply.github.com>
2018-09-12 15:49:08 +0200

    just copied the conda forge bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,25 +4,28 @@ cd build
 :: Set environment variables.
 set HDF5_EXT_ZLIB=zlib.lib
 
+set "CXXFLAGS=%CXXFLAGS% -LTCG"
+
 :: Configure step.
-cmake -G "NMake Makefiles" ^
+cmake -G "Ninja" ^
       -D CMAKE_BUILD_TYPE:STRING=RELEASE ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -D CMAKE_PREFIX_PATH:PATH=%LIBRARY_PREFIX% ^
       -D CMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-      -D HDF5_BUILD_CPP_LIB=ON ^
+      -D HDF5_BUILD_CPP_LIB:BOOL=ON ^
+      -D CMAKE_POSITION_INDEPENDENT_CODE:BOOL=ON ^
       -D BUILD_SHARED_LIBS:BOOL=ON ^
-      -D HDF5_BUILD_HL_LIB=ON ^
+      -D HDF5_BUILD_HL_LIB:BOOL=ON ^
       -D HDF5_BUILD_TOOLS:BOOL=ON ^
       -D HDF5_ENABLE_Z_LIB_SUPPORT:BOOL=ON ^
-      -D HDF5_ENABLE_THREADSAFE=ON ^
-      -D ALLOW_UNSUPPORTED=ON ^
+      -D HDF5_ENABLE_THREADSAFE:BOOL=ON ^
+      -D ALLOW_UNSUPPORTED:BOOL=ON ^
       %SRC_DIR%
 if errorlevel 1 exit 1
 
 :: Build C libraries and tools.
-nmake
+ninja
 if errorlevel 1 exit 1
 
 :: Install step.
-nmake install
+ninja install
 if errorlevel 1 exit 1
